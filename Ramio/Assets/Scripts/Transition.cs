5bbcab6daedb145cc2c057e58cb84a7bbe8e9494
Unity Man <40305575+UnityManOfficial@users.noreply.github.com>
2021-12-03 09:01:11 -0800

    Update V0.23
    -Added cinemachine placeholders
    -Removed filters from start and end transition sprites

--- Ramio/Assets/Scripts/Transition.cs
+++ Ramio/Assets/Scripts/Transition.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Transition : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Animator myAnimator;
+
     void Start()
     {
-        
+        myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.layer == 3)
+        {
+            myAnimator.SetBool("NextLevel", true);
+        }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            myAnimator.SetBool("NextLevel", false);
+        }
+    }
+
 }
