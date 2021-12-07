b447e0e197550ef8c6c3e7bb78305bfecabbb80d
Unity Man <40305575+UnityManOfficial@users.noreply.github.com>
2021-12-07 08:35:06 -0800

    Update V0.252
    -Updated Lever Prefab location
    -Added SceneKeeper Placeholder Script

--- Ramio/Assets/Scripts/SceneKeeper.cs
+++ Ramio/Assets/Scripts/SceneKeeper.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;  
 
 public class SceneKeeper : MonoBehaviour
 {
+    int startingSceneIndex;
+
+    private void Awake()
+    {
+        int numSceneKeeper = FindObjectsOfType<SceneKeeper>().Length;
+        if (numSceneKeeper > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
     }
 }
