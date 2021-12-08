3d57fbb7bc9562505c341d020f584d71234693ae
Unity Man <40305575+UnityManOfficial@users.noreply.github.com>
2021-12-08 09:09:28 -0800

    Update V0.256
    -Updated Health Script
    -Adjusted UI

--- Ramio/Assets/Scripts/Health.cs
+++ Ramio/Assets/Scripts/Health.cs
@@ -7,16 +7,21 @@ public class Health : MonoBehaviour
 {
 
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
 
     public void SetMaxHealth(int HP)
     {
         slider.maxValue = HP;
         slider.value = HP;
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int HP)
     {
         slider.value = HP;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
 
