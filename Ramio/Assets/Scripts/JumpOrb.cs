b734d7bb45349c32491dacfc5a3bed9c48a4e64e
aaronDavis90 <92536465+aaronDavis90@users.noreply.github.com>
2021-12-14 10:17:17 -0800

    Added Jump reset with a jump orb
    If touched, the orb will be inactive for 5 seconds.
    *This will only work if double jump script is attached to the player.

---
+++ Ramio/Assets/Scripts/JumpOrb.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpOrb : MonoBehaviour
+{
+    private bool JumpOrbActive;
+    private float enableTimer;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        JumpOrbActive = true;
+        spriteRenderer = transform.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (!JumpOrbActive)
+        {
+            enableTimer -= Time.deltaTime;
+
+            if (enableTimer < 0)
+            {
+                JumpOrbActive = true;
+                spriteRenderer.color = new Color(1, 1, 1, 1);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DoubleJump player = collision.gameObject.GetComponent<DoubleJump>();
+        if (JumpOrbActive && player != null)
+        {
+            JumpOrbActive = false;
+            enableTimer = 5f;
+            spriteRenderer.color = new Color(1, 1, 1, .5f);
+            player.TouchedJumpOrb();
+        }
+    }
+}
